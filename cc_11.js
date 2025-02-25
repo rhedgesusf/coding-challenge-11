1579627fb49a98b74fceb6ac7be9a1d247c5f203
Riley Hedges <rhedges1@usf.edu>
2025-02-25 17:15:38 -0500

    Task 2 - Created Borrower Class

--- cc_11.js
+++ cc_11.js
@@ -32,3 +32,39 @@ console.log(book1.getDetails());
 
 book1.updateCopies(-1);
 console.log(book1.getDetails());
+
+///////////////////////////////////////
+// Task 2: Creating a Borrower Class //
+///////////////////////////////////////
+
+console.log("--------------------------------------");
+console.log("Task 2: Creating a Borrower Class");
+
+class Borrower {
+    constructor(name, borrowerId) {
+        this.name = name
+        this.borrowerId = borrowerId
+        this.borrowedBooks = []
+    }
+    
+    borrowBook(book) {
+        this.borrowedBooks.push(book)
+    }
+    returnBook(book) {
+        let index = this.borrowedBooks.findIndex((element) => element === book);
+
+        if (index == -1){
+            console.log("Book Not Found");
+        }
+        else {
+           this.borrowedBooks.splice(index, 1);
+        }
+    }
+}
+
+const borrower1 = new Borrower("Alice Johnson", 201);
+borrower1.borrowBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);
+
+borrower1.returnBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);
